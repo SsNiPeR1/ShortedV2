31f15f71378b353cd60b78dba01575f3eac80dc6
Konstantin-create <schahov.konst@gmail.com>
2022-08-23 13:22:45 +0300

    Worked on project structure

---
+++ app/config.py
@@ -0,0 +1,8 @@
+class Config:
+    vanity_urls = {
+        "resin": "https://explorer.resincoin.ml",
+        "ssniper1": "https://ssniper1.ml",
+        "murka": "https://murkauserbot.ml",
+        "gay": "https://t.me/gdlbo",
+    }
+    expression = "^(https?:\/\/)?[a-zA-Z0-9_\-]+(\.[a-zAZ0-9_\-]+)+(\/.*)?$"
